766e9a89e430c77b10911f69adf6a588aae70f8f
the-dev-d <dhaivathl@gmail.com>
2024-07-14 10:35:05 +0530

    backend

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 const config = {
     // BACKEND_URL: "https://mirror8.gieom.com"
-    BACKEND_URL: "https://mirror9.gieom.com"
+    BACKEND_URL: "https://mirror8.gieom.com"
 
 }
 export default config;
