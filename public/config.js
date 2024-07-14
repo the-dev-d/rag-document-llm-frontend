b60c0574e66160a86c279891e67b51f68b3e2572
the-dev-d <dhaivathl@gmail.com>
2024-07-14 11:03:53 +0530

    named stable

--- public/config.js
+++ public/config.js
@@ -1,6 +1,5 @@
 const config = {
-    // BACKEND_URL: "https://mirror8.gieom.com"
-    BACKEND_URL: "https://mirror8.gieom.com"
+    BACKEND_URL: "https://mirror9.gieom.com"
 
 }
 export default config;
