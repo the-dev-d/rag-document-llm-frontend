f3f7fc5ee387155748a8d4f2aba2ec232b0f2e54
the-dev-d <dhaivathl@gmail.com>
2024-07-31 16:04:15 +0530

    tooltip

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
   import type { Chat } from '@/types/type'
   import { sidebar } from '@/utils/GlobalStates'
-  import { nextTick, onUnmounted, ref, watch } from 'vue'
+  import { nextTick, onMounted, onUnmounted, ref, watch } from 'vue'
   import socket from '@/utils/ChatService'
   import { parseMarkDown } from '@/utils/ResponseParser'
+import { initFlowbite, initTooltips } from 'flowbite'
 
   onUnmounted(() => {
     sidebar.status.value = true;
+  })
+
+  onMounted(() => {
+    initFlowbite()
   })
 
   const loading = ref(0);
   const prompt = ref('')
   const chatSection = ref<HTMLElement|null>(null);
   const chats = ref<Chat[]>([])
+  const copyTooltip = ref("Copy");
   const  suggested_prompt = socket.SUGGESTED_PROMPTS;
   //   {
   //   role:'bot',
@@ -22,17 +28,23 @@
   async function handleDownload() {
 
     const content = chats.value[1].message;
-    const popup = window.open('', "content", "'height=1920,width=1080'")
+    const popup = window.open('', "c", "'height=1920,width=1080'")
+    console.log(popup);
     if(!popup)
       return;
 
-    popup.document.body.innerHTML = content;
+    popup.document.body.innerHTML = parseMarkDown(content);
     popup.print();
-    popup.close();
+    popup.onafterprint = () => popup.close();
+    //popup.close();
   }
 
   async function handleCopy() {
     navigator.clipboard.writeText(chats.value[1].message);
+    if(copyTooltip.value === "Copy") {
+      copyTooltip.value = "Copied ✔️";
+      setTimeout(() => {copyTooltip.value = "Copy"}, 2000)
+    }
   }
 
   async function sendCurrentPrompt() {
@@ -79,6 +91,7 @@
 
   watch(chats, async (val) => {
     nextTick(() => {
+        initTooltips();
         if(chatSection.value)
           chatSection.value.scrollTop = chatSection.value.scrollHeight;
       })
@@ -95,18 +108,26 @@
       <div class="mx-auto flex gap-3 items-center">
         <textarea :disabled="!!chats.length" v-model="prompt" v-on:keyup="handleClicks" id="message" rows="4" class="block p-2.5 flex-1 border-2 text-sm text-gray-900 bg-gray-50 rounded-lg border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 resize-none dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Incident Information"></textarea>
         <div class="grid gap-2">
-          <button :disabled="!!chats.length || prompt.trim() == ''" v-on:click="sendCurrentPrompt" type="button" class="text-white bg-dark-primary-medium hover:bg-dark-primary-darker focus:ring-4 h-fit focus:outline-none disabled:bg-gray-600 focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 ">
+          <button data-tooltip-target="tooltip-send" :disabled="!!chats.length || prompt.trim() == ''" v-on:click="sendCurrentPrompt" type="button" class="text-white bg-dark-primary-medium hover:bg-dark-primary-darker focus:ring-4 h-fit focus:outline-none disabled:bg-gray-600 focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 ">
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
             <span class="sr-only">Send Message</span>
           </button>
-          <button @click="() => (chats = [])" :disabled="(chats.length%2)!==0" type="button" class="text-white bg-dark-primary-medium hover:bg-dark-primary-darker focus:ring-4 h-fit focus:outline-none disabled:bg-gray-600 focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 ">
+          <div id="tooltip-send" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+            Send
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
+          <button @click="() => (chats = [])" data-tooltip-target="tooltip-reset" :disabled="(chats.length%2)!==0" type="button" class="text-white bg-dark-primary-medium hover:bg-dark-primary-darker focus:ring-4 h-fit focus:outline-none disabled:bg-gray-600 focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 ">
             <span class="material-symbols-outlined">
               restart_alt
             </span>
             <span class="sr-only">Send Message</span>
           </button>
+          <div id="tooltip-reset" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+            Reset
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
         </div>
       </div>
       <div class="flex gap-1 md:gap-3 py-2 md:flex-row flex-col">
@@ -140,13 +161,21 @@
             class="relative p-3 rounded-md w-fit chat"
             v-html="chat.role == 'bot' ? parseMarkDown(chat.message) : chat.message">
             </div>
-            <div class="ml-5 mt-3 flex gap-2" v-if="chat.role == 'bot'">
-              <button @click="handleDownload" class="w-fit px-4 py-1 rounded-md bg-dark-primary-medium text-white hover:bg-dark-primary-darker">
+            <div class="ml-5 mt-3 flex gap-2 relative" v-if="chat.role == 'bot'">
+              <button data-tooltip-target="tooltip-download" @click="handleDownload" class="w-fit px-4 py-1 rounded-md bg-dark-primary-medium text-white hover:bg-dark-primary-darker">
                 <i class="fa-solid fa-download"></i>
               </button>
-              <button @click="handleCopy" class="w-fit px-4 py-1 rounded-md bg-dark-primary-medium text-white hover:bg-dark-primary-darker">
+              <div id="tooltip-download" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                Download
+                <div class="tooltip-arrow" data-popper-arrow></div>
+              </div>
+              <button data-tooltip-target="tooltip-copy" @click="handleCopy" class="w-fit px-4 py-1 rounded-md bg-dark-primary-medium text-white hover:bg-dark-primary-darker">
                 <i class="fa-solid fa-clipboard"></i>
               </button>
+              <div id="tooltip-copy" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                {{copyTooltip}}
+                <div class="tooltip-arrow" data-popper-arrow></div>
+              </div>
             </div>
           </div>
           <div v-if="chat.role == 'user'" class="grid w-8 h-8 text-white rounded-full shadow-sm bg-bubble-user place-items-center">
